f7bd28080d0d156eee75588b7fcb702899ff8552
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-05-26 15:19:42 +0000

    Add hash method to HashMap class

--- hashMap.js
+++ hashMap.js
@@ -2,4 +2,16 @@ class HashMap {
   constructor(initialSize = 16) {
     this.buckets = new Array(initialSize).fill(null).map(() => []);
   }
+
+  hash(key) {
+    let hashCode = 0;
+    let bucketSize = this.buckets.length;
+    const primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = ((primeNumber * hashCode) + key.charCodeAt(i)) % bucketSize;
+    }
+
+    return hashCode;
+  }
 }
