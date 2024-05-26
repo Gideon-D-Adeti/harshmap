a26ae67d15c59d193dc0f0c9c96f019ee350e0d5
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-05-26 15:33:44 +0000

    Add set method to HashMap class

--- hashMap.js
+++ hashMap.js
@@ -1,17 +1,42 @@
 class HashMap {
   constructor(initialSize = 16) {
     this.buckets = new Array(initialSize).fill(null).map(() => []);
+    this.size = 0;
+    this.loadFactor = 0.75;
   }
 
+  //   Method to generate hashCodes of keys
   hash(key) {
     let hashCode = 0;
     let bucketSize = this.buckets.length;
     const primeNumber = 31;
 
     for (let i = 0; i < key.length; i++) {
-      hashCode = ((primeNumber * hashCode) + key.charCodeAt(i)) % bucketSize;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % bucketSize;
     }
 
     return hashCode;
+  }
+
+  // Method to set key-value pairs
+  set(key, value) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [k, v] = bucket[i];
+      if (k === key) {
+        bucket[i] = [key, value];
+        return;
+      }
+    }
+
+    bucket.push([key, value]);
+    this.size++;
+
+    if (this.size / this.buckets.length > this.loadFactor) {
+      // TODO: write resize method
+      return;
+    }
   }
 }
